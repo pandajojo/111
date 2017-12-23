2681c5029d3946a1f3eee003a2d80eff3e2c2580
panda <395367590@qq.com>
2017-12-23 17:52:37 +0800

    ui

---
+++ weixing/js/reg.js
@@ -0,0 +1,29 @@
+(function($){
+
+
+    $(function() {
+
+
+
+        
+            $("#birth").datepicker({
+                dateFormat: 'yy-mm-dd',
+                yearRange: '1917:2017',
+                changeMonth: true,
+                changeYear: true
+            });
+
+     
+        
+
+
+
+
+
+
+
+
+
+    })
+
+})(jQuery)
